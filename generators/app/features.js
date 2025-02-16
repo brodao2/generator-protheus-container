c2ffe9dccd2a4e1ec2ff3b5bae1a8d0d4472a1f5
Alan Candido <brodao@gmail.com>
2025-02-16 10:44:51 -0300

    Update features.js

--- generators/app/features.js
+++ generators/app/features.js
@@ -98,8 +98,7 @@ module.exports = {
       return feature.id === featureId;
     }).forEach((feature) => {
       feature.urls.forEach((url) => {
-        const credential = encodeURI(`${user}:${password}`)
-        // .replaceAll("@", "%40")
+        const credential = encodeURI(`${user}:${password}`).replaceAll("@", "%40")
         // .replaceAll("!", "%40");
 
         result.push(
